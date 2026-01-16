3b227c80fa18695804552963b97aa2723e5b489e
Jelle Gemert <jeleeinfo@gmail.com>
2026-01-16 15:30:54 +0100

    made steer lerp + reset after letting go for a smoother visual

--- Assets/Scripts/SteeringWheelController.cs
+++ Assets/Scripts/SteeringWheelController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class SteeringWheelController : MonoBehaviour
 {
     public InputActionReference Steer;
     [SerializeField] float _maxRotationAngle = 30f;
+    [SerializeField] float _returnSpeed = 12f;
     [SerializeField] InputSettings _inputSettings;
 
     private void OnEnable()
@@ -23,14 +25,27 @@ public class SteeringWheelController : MonoBehaviour
 
         float steerValue = -leftStick.x;
 
-        if (Mathf.Abs(steerValue) > _inputSettings.DeadZone)
-        {
-            float rotationZ = steerValue * _maxRotationAngle;
-            transform.localEulerAngles = new Vector3(
-                transform.localEulerAngles.x,
-                transform.localEulerAngles.y,
-                rotationZ
+        if (Mathf.Abs(steerValue) < _inputSettings.DeadZone)
+            steerValue = 0f;
+        
+
+            float targetRotationZ = steerValue * _maxRotationAngle;
+
+            float currentRotationZ = transform.localEulerAngles.z;
+            if (currentRotationZ > 180f)
+                currentRotationZ -= 360f;
+
+            float newRotationZ = Mathf.Lerp(
+                currentRotationZ,
+                targetRotationZ,
+                Time.deltaTime * _returnSpeed
             );
-        }
+
+            transform.localEulerAngles = new Vector3(
+            transform.localEulerAngles.x,
+            transform.localEulerAngles.y,
+            newRotationZ
+         );
+        
     }
 }
