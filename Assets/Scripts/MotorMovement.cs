64ea459938c8e5540a513496a221e17869a8e2e2
Jelle Gemert <jeleeinfo@gmail.com>
2026-01-24 12:58:22 +0100

    refactored movement into seperate methods

--- Assets/Scripts/MotorMovement.cs
+++ Assets/Scripts/MotorMovement.cs
@@ -12,61 +12,79 @@ public class MotorMovement : MonoBehaviour
     [SerializeField] SteeringWheelController _steeringWheel;
     [SerializeField] Transform _frontWheel;
     [SerializeField] Transform _rearWheel;
+    [SerializeField] InputSettings _inputSettings;
 
     [Header("Variables")]
     [SerializeField] float _motorForce = 100f;
     [SerializeField] float _steerSpeed = 40f;
     [SerializeField] float _maxSteerAngle = 45f;
     [SerializeField, Range(1.5f, 3f)] float _breakModifier = 2f;
-    [SerializeField] InputSettings _inputSettings;
+    
     private float _brakeForce;
+    private float _accelerationInput;
+    private float _brakeInput;
+    private float _steerInput;
+    private Vector3 _moveDirection;
+
     private Rigidbody _rb;
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _brakeForce = _motorForce * _breakModifier;
     }
-
-    void HandleAcceleratiom()
+    void ReadInput()
     {
-
+        _accelerationInput = Accelerate.action.ReadValue<float>();
+        _brakeInput = Brake.action.ReadValue<float>();
+        _steerInput = -_steeringWheel.CurrentSteerValue;
     }
-    void FixedUpdate()
+
+    void HandleSteering()
     {
-        float accelerationPressed = Accelerate.action.ReadValue<float>();
-        float brakePressed = Brake.action.ReadValue<float>(); 
-        float steerValue = Steer.action.ReadValue<Vector2>().x;
+        _moveDirection =
+    Quaternion.Euler(0f, _steerInput * _maxSteerAngle, 0f)
+    * transform.forward;
 
-        float steerZ = -_steeringWheel.CurrentSteerValue;
-      
+        _moveDirection.y = 0f;
+        _moveDirection.Normalize();
 
-        Vector3 forward = Quaternion.Euler(0f, steerZ * _maxSteerAngle, 0f) * transform.forward;
-        forward.y = 0f;
-        forward.Normalize();
+        Quaternion targetRotation =
+            Quaternion.LookRotation(_moveDirection, Vector3.up);
 
+        _rb.MoveRotation(
+            Quaternion.RotateTowards(
+                _rb.rotation,
+                targetRotation,
+                _steerSpeed * Time.fixedDeltaTime
+            )
+        );
+    }
+    void HandleAcceleration()
+    {
+        if (_accelerationInput <= 0f)
+            return;
 
-
-
-        Quaternion targetRotation = Quaternion.LookRotation(forward, Vector3.up);
-        _rb.MoveRotation(Quaternion.RotateTowards(_rb.rotation, targetRotation, _steerSpeed * Time.fixedDeltaTime));
-
+        _rb.AddForce(_moveDirection * _motorForce, ForceMode.Force);
+    }
+    void HandleBrake()
+    {
+        if (_brakeInput <= 0f)
+            return;
 
         Vector3 velocity = _rb.linearVelocity;
 
-        if (accelerationPressed > 0f)
-        {
-            _rb.AddForce(forward * _motorForce, ForceMode.Force);
-            Debug.Log("Move");
-        }
+        if (velocity.sqrMagnitude < 0.01f)
+            return;
 
-        if (brakePressed > 0f && velocity.sqrMagnitude > 0.01f)
-        {
-            Vector3 brakeForce = -velocity.normalized * _brakeForce;
-            _rb.AddForce(brakeForce, ForceMode.Force);
-            Debug.Log("Stopping...");
-        }
-
-        
+        Vector3 brakeForce = -velocity.normalized * _brakeForce;
+        _rb.AddForce(brakeForce, ForceMode.Force);
+    }
+    void FixedUpdate()
+    {
+        ReadInput();
+        HandleSteering();
+        HandleAcceleration();
+        HandleBrake();
     }
     private void OnEnable()
     {
