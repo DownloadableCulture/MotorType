551441c40d78f1ac0b15b2f0eab540d8bb95aaa6
Jelle Gemert <jeleeinfo@gmail.com>
2026-08-17 14:49:35 +0200

    wheels rotate according to speed instead of continious

--- Assets/Scripts/WheelRotation.cs
+++ Assets/Scripts/WheelRotation.cs
@@ -4,18 +4,34 @@ public class WheelRotation : MonoBehaviour
 {
     [SerializeField] Transform _wheel;
     [SerializeField] float _rotationSpeed = 360f;
+    private MotorMovement _motorMovement;
     
     void Awake()
     {
         if (_wheel == null)
         {
-           _wheel = GetComponent<Transform>();
+            _wheel = GetComponent<Transform>();
+        }
+
+        // Get the MotorMovement component from the parent or this GameObject
+        _motorMovement = GetComponentInParent<MotorMovement>();
+        if (_motorMovement == null)
+        {
+            _motorMovement = GetComponent<MotorMovement>();
         }
     }
 
-
     void Update()
     {
-        _wheel.Rotate(Vector3.right * _rotationSpeed * Time.deltaTime);
+        if (_motorMovement != null)
+        {
+            float speed = _motorMovement.CurrentSpeed;
+            _wheel.Rotate(Vector3.right * speed * _rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Fallback to constant rotation if MotorMovement is not found
+            _wheel.Rotate(Vector3.right * _rotationSpeed * Time.deltaTime);
+        }
     }
 }
